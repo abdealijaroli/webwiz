2b2cca7e02c3943bf99a04c2885ab0c92b608aeb
abdealijaroli <abdealiburu03@gmail.com>
2023-04-25 00:02:42 +0530

    frontend work

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">{children}</body>
+      <body className=" bg-primary text-secondary">{children}</body>
     </html> 
 
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
-  return (
-    <main className="text-center text-xl m-10 font-medium">
-        <h1>hello world</h1>
-    </main>
-  )
+    return (
+        <main className="text-center text-xl m-6 font-medium flex flex-row">
+            <div className="flex flex-col items-start justify-start border w-3/4 h-screen m-2 rounded-xl">
+                <h1 className="m-2">Canvas</h1>
+            </div>
+
+            <div className="flex flex-col items-start justify-start border w-1/4 h-screen m-2 rounded-xl">
+                <h1 className="m-2 float-left">Chat</h1>
+            </div>
+        </main>
+    );
 }
