491d35b0d12844e78a6f6ae8f9e0f834ab3792dc
abdealijaroli <abdealiburu03@gmail.com>
2023-06-30 13:49:07 +0530

    Revert "Revert "� proof of concept ready!""
    This reverts commit 5f8fbdfa23e5fa887749e14331e0a19080591d44.

--- app/Canvas.tsx
+++ app/Canvas.tsx
@@ -1,7 +1,21 @@
-export default function Canvas({ }) {
-    return (
-        <>
-            <div className="flex flex-col items-start justify-start w-3/4 m-2 rounded-xl bg-white"></div>
-        </>
-    );
+import React from "react";
+
+interface CanvasProps {
+    html?: string;
+    css?: string;
+    js?: string;
 }
+
+const Canvas = ({ html, css, js }: CanvasProps) => {
+    return (
+        <div className="flex flex-col items-center justify-start w-3/4 m-2 p-3 rounded-xl bg-white">
+            <div dangerouslySetInnerHTML={{ __html: html || "(Ask the AI to build something for you. Preview the changes on this canvas, LIVE!)" }} />
+
+            <style>{css}</style>
+
+            <script>{js}</script>
+        </div>
+    );
+};
+
+export default Canvas;
